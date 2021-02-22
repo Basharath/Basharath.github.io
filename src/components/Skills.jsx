15663f569ad9d24ce1c5bc80fdfcbfc9f44b0d6b
Basharath <basharath.in@gmail.com>
2021-02-22 18:17:08 +0530

    Updates skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,19 +9,16 @@ export default function Skills() {
         <span className="skill">CSS/SASS</span>
         <span className="skill">JavaScript</span>
         <span className="skill">React</span>
-        <span className="skill">React Native</span>
         <span className="skill">Node</span>
-        <span className="skill">Python</span>
-        <span className="skill">Google AppScript</span>
-        <span className="skill">Bootstrap</span>
-        <span className="skill">Figma</span>
-        <span className="skill">Photoshop</span>
-        <span className="skill">After Effects</span>
+        <span className="skill">Express</span>
         <span className="skill">Git</span>
-        <span className="skill">Gatsby</span>
+        <span className="skill">MongoDB</span>
+        <span className="skill">Figma</span>
+        <span className="skill">Bootstrap</span>
+        <span className="skill">Google AppScript</span>
         <span className="skill">NextJS</span>
-        <span className="skill">Bash</span>
-        <span className="skill">Wordpress</span>
+        <span className="skill">Gatsby</span>
+        <span className="skill">Photoshop</span>
         <span className="skill">Outreach</span>
         <span className="skill">SEO</span>
       </div>
